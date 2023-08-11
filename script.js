21ffc176395d5874c04e7f5a2c77f292a8ec0234
falsumit <brooks516@outlook.com>
2023-08-11 17:35:56 -0400

    rough draft of grid with style complete, still need to figure out grid scaling and maintaining aspect ratio of grid squares

--- script.js
+++ script.js
@@ -2,30 +2,35 @@ const pageContainer = document.createElement('div');
 pageContainer.setAttribute('id', 'page-container');
 document.body.appendChild(pageContainer);
 
+const popBtn = document.createElement('button');
+popBtn.setAttribute('id', 'popup-button');
+popBtn.textContent = 'Generate New Grid';
+pageContainer.appendChild(popBtn);
+
 const gridContainer = document.createElement('div');
 gridContainer.setAttribute('id', 'grid-container');
 pageContainer.appendChild(gridContainer);
 
-gridContainer.insertAdjacentHTML('beforeend', `
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-    <div class="grid-item"></div>
-`);
+for (let i = 0; i < 256; i++) {
+    gridContainer.insertAdjacentHTML('beforeend', `
+        <div class="grid-item"></div>
+    `);
+}
 
-// const gridItem = document.createElement('div');
-// gridItem.setAttribute('class', 'grid-item');
-// gridContainer.appendChild(gridItem);
+const gridItems = document.getElementsByClassName('grid-item');
 
+function getUserInput() {
+    userInput = parseInt(window.prompt('How many squares per side do you want the new grid to be?', 'Value must be between 2 and 256'));
+}
+
+popBtn.addEventListener('click', () => {
+    getUserInput();
+    if (userInput >= 2 && userInput <= 256) {
+        gridContainer.innerHTML = '';
+        for (let i = 0; i < userInput; i++) {
+            gridContainer.insertAdjacentHTML('beforeend', `
+                <div class="grid-item"></div>
+            `);
+        }
+    }
+});
