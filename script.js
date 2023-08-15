d7d110dd7ba65a9c158b03b98c060a8c490c2d21
falsumit <brooks516@outlook.com>
2023-08-15 15:13:45 -0400

    fully built game with functioning buttons

--- script.js
+++ script.js
@@ -1,46 +1,43 @@
-const pageContainer = document.createElement('div');
-pageContainer.setAttribute('id', 'page-container');
-document.body.appendChild(pageContainer);
+const pageContainer = document.getElementById('page-container');
+const getSize = document.getElementById('get-size');
+const reset = document.getElementById('reset');
+const pixelGrid = document.getElementById('pixel-grid');
+let pixels = pixelGrid.querySelectorAll('#pixel-grid > div');
 
-const popupButton = document.createElement('button');
-popupButton.setAttribute('id', 'popup-button');
-popupButton.textContent = 'Generate New Grid';
-pageContainer.appendChild(popupButton);
-
-const gridContainer = document.createElement('div');
-gridContainer.setAttribute('id', 'grid-container');
-pageContainer.appendChild(gridContainer);
+let size = 16;
 
 function makeGrid(size) {
-    gridContainer.innerHTML = '';
+    pixelGrid.innerHTML = '';
     for (gridSize = 0; gridSize < size * size; gridSize++) {
-        let gridItem = document.createElement('div');
-        gridItem.setAttribute('class', 'grid-item');
-        gridContainer.appendChild(gridItem);
-        gridContainer.style.cssText = `grid-template: repeat(${size}, 1fr) / repeat(${size}, 1fr);`;
+        let pixel = document.createElement('div');
+        pixel.classList.toggle('pixel');
+        pixelGrid.appendChild(pixel);
+        pixelGrid.style.gridTemplate = `repeat(${size}, 1fr) / repeat(${size}, 1fr)`;
     }
 }
+makeGrid(size);
 
-makeGrid(16);
-
-let userInput;
-
-function getUserInput() {
-    userInput = parseInt(window.prompt('How many squares per side do you want the new grid to be?', 'Value must be between 2 and 50'));
+function colorPixels() {
+    pixels = pixelGrid.querySelectorAll('#pixel-grid > div');
+    pixels.forEach((item) => {
+        const pixel = item;
+        pixel.addEventListener('mouseover', () => {
+            pixel.classList.add('pixel-color');
+        });
+    });
 }
+colorPixels();
 
-popupButton.addEventListener('click', () => {
-    getUserInput();
-    if (userInput >= 2 && userInput <= 50) {
-        size = userInput;
+getSize.addEventListener('click', () => {
+    size = parseInt(window.prompt('How big do you want the new grid to be?', 'Must be between 2 and 50'));
+    if (size >= 2 && size <= 50) {
         makeGrid(size);
+        colorPixels();
     }
 });
 
-let gridChildren = gridContainer.getElementsByClassName('grid-item');
-
-for (let i = 0; i < gridChildren.length; i++) {
-    gridChildren[i].addEventListener('mouseover', (event) => {
-        event.target.style.cssText = 'background-color: black;';
-    });
-}
+reset.addEventListener('click', () => {
+    pixelGrid.innerHTML = '';
+    makeGrid(size);
+    colorPixels();
+});
